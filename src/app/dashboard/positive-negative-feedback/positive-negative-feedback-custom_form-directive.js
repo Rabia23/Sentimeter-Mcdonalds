23c5e45165b3b19d2aa4b41d9eb4209cd7472d0a
abdullah <abdullah.khan.niazi@hotmail.com>
2016-03-08 17:29:28 +0500

    added js for changing height

--- src/app/dashboard/positive-negative-feedback/positive-negative-feedback-custom_form-directive.js
+++ src/app/dashboard/positive-negative-feedback/positive-negative-feedback-custom_form-directive.js
@@ -10,7 +10,30 @@
         link: function(scope, ele, attrs){
           scope.$watch('comments', function(watchedComments) {
             if(watchedComments !== undefined){
+              modalHeight();
               window.initCustomForms();
+              $(window).on("resize", function() {
+                modalHeight();
+              });
+            }
+
+            function modalHeight(){
+              var elemHolder = $('.modal-1200 .modal-content').height();
+              var header = $('.modal-1200 .header-holder').height();
+              var height = elemHolder - header;
+              var content = $('.modal-1200 .modal-detail');
+              var tableHolder = $('.modal-1200 .table-block').height();
+              var tableHeader = $('.modal-1200 .table-header').height();
+              var tableHeight = tableHolder - tableHeader;
+              var tableContent = $('.modal-1200 .content');
+
+              content.css({
+              'height' : height
+              });
+
+              tableContent.css({
+              'height' : tableHeight
+              });
             }
           });
 
