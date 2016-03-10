8b57e3f52f02600a72e91a171c94791f25c2e9a8
abdullah <abdullah.khan.niazi@hotmail.com>
2016-03-10 11:08:46 +0500

    moved the feedback code to service

--- src/app/dashboard/category-performance-analysis/_category-performance-analysis.js
+++ src/app/dashboard/category-performance-analysis/_category-performance-analysis.js
@@ -1,6 +1,7 @@
 (function() {
   angular.module( 'livefeed.dashboard.category_performance_analysis', [
     'livefeed.category_performance_analysis.api',
+    'livefeed.dashboard.category_performance_analysis.feedback_service',
     'helper_factories',
     'ui.bootstrap',
     'chart.js'
