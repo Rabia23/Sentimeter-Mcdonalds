5f1b28ba39a7bd09b1dcff5a970a9aa8a6cf97d5
rabia23 <rabia.iftikhar@arbisoft.com>
2016-03-17 17:20:48 +0500

    fixed donut height issue in business segment breakdown

---
+++ src/app/dashboard/category-performance-analysis/category-performance-sameSegmentHeight-directive.js
@@ -0,0 +1,20 @@
+(function() {
+  angular.module('livefeed.dashboard.category_performance_analysis')
+  .directive('sameSegmentHeight', function() {
+    return {
+      restrict: 'A',
+      scope: {
+        data: '='
+      },
+      link: function(scope, ele, attrs) {
+        scope.$watch('data', function(watchedData) {
+          if(watchedData !== undefined){
+            console.log("same segment height");
+            console.log(watchedData);
+            window.initSameHeight();
+          }
+        });
+      }
+    };
+  });
+})();
