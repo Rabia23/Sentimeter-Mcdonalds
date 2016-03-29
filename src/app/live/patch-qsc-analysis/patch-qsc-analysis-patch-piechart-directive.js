8a572aaf407249db1ada31ca02b9ab2555ea8cd8
abdullah <abdullah.khan.niazi@hotmail.com>
2016-03-29 14:45:58 +0500

    fixed remaining files identation

--- src/app/live/patch-qsc-analysis/patch-qsc-analysis-patch-piechart-directive.js
+++ src/app/live/patch-qsc-analysis/patch-qsc-analysis-patch-piechart-directive.js
@@ -1,57 +1,57 @@
 (function() {
 
   angular.module( 'livefeed.live.patch_qsc_analysis')
-    .directive('patchPieChart', function() {
-      return {
-        restrict: 'A',
-        scope: {
-           data: '='
-        },
-        link: function(scope, ele, attrs) {
-            var graph_drawn = false;
-            var live_piechart;
-            scope.$watch('data', function(watchedData) {
+  .directive('patchPieChart', function() {
+    return {
+      restrict: 'A',
+      scope: {
+        data: '='
+      },
+      link: function(scope, ele, attrs) {
+        var graph_drawn = false;
+        var live_piechart;
+        scope.$watch('data', function(watchedData) {
+          if(watchedData !== undefined){
+            var data = scope.data;
+            if(!graph_drawn){
+              live_piechart = AmCharts.makeChart("piechart",
+              {
+                "type": "pie",
+                "startDuration": 0,
+                "angle": 25,
+                "pullOutDuration": 0,
+                "pullOutRadius": 0,
+                "balloonText": "[[title]]<br><span style='font-size:14px'><b>[[value]]</b> ([[percents]]%)</span>",
+                "depth3D": 55,
+                "labelRadius": -105,
+                "labelText":  "[[value]]",
+                "color": "#FFFFFF",
+                "fontFamily": "'Oswald', sans-serif",
+                "fontSize": 25,
+                "marginTop":"3%",
+                "marginRight":0,
+                "marginLeft":0,
+                "marginBottom":"18%",
+                "autoMargins":false,
+                "titleField": "category",
+                "valueField": "column-1",
+                "colorField": "color",
+                "allLabels": [],
+                "balloon": {},
+                "titles": [],
+                "dataProvider": data
+              });
+              graph_drawn = true;
+            }
+            else {
+              live_piechart.dataProvider = data;
+              live_piechart.validateData();
+            }
 
-              if(watchedData !== undefined){
-                var data = scope.data;
-                if(!graph_drawn){
-                  live_piechart = AmCharts.makeChart("piechart",
-                  {
-                      "type": "pie",
-                      "startDuration": 0,
-                      "angle": 25,
-                      "pullOutDuration": 0,
-                      "pullOutRadius": 0,
-                      "balloonText": "[[title]]<br><span style='font-size:14px'><b>[[value]]</b> ([[percents]]%)</span>",
-                      "depth3D": 55,
-                      "labelRadius": -105,
-                      "labelText":  "[[value]]",
-                       "color": "#FFFFFF",
-                       "fontFamily": "'Oswald', sans-serif",
-                      "fontSize": 25,
-                      "marginTop":"3%",
-                      "marginRight":0,
-                      "marginLeft":0,
-                      "marginBottom":"18%",
-                      "autoMargins":false,
-                      "titleField": "category",
-                      "valueField": "column-1",
-                      "colorField": "color",
-                      "allLabels": [],
-                      "balloon": {},
-                      "titles": [],
-                      "dataProvider": data
-                  });
-                  graph_drawn = true;
-                }
-                else {
-                  live_piechart.dataProvider = data;
-                  live_piechart.validateData();
-                }
+          }
+        });
+      }
+    };
+  });
 
-              }
-            });
-        }
-      };
-    });
 })();
