cf95907682e6b970ffeb4043fc5dbda70c4f3e5d
abdullah <abdullah.khan.niazi@hotmail.com>
2016-03-29 14:43:43 +0500

    _path-qsc files identation fixed

--- src/app/live/patch-qsc-analysis/patch-qsc-analysis-controller.js
+++ src/app/live/patch-qsc-analysis/patch-qsc-analysis-controller.js
@@ -6,11 +6,10 @@
 
     function region_data(action_analysis_data){
       var complaints = {};
-       _.each(action_analysis_data, function(action){
-
-           complaints[Global.complaintAnalysisAction[action.action_taken][0]] = action.count;
-       });
-       return complaints;
+      _.each(action_analysis_data, function(action){
+        complaints[Global.complaintAnalysisAction[action.action_taken][0]] = action.count;
+      });
+      return complaints;
     }
 
     function patch_qsc_analysis() {
@@ -34,11 +33,11 @@
         data.data.feedback_count = data.data.feedback_count - count;
       });
       _.map($scope.complaint_view, function(data){
-         data.data.action_analysis = _.filter(data.data.action_analysis,  function(dat) { return dat.action_taken !== ComplaintStatusEnum.get_skip_label_index(); });
+        data.data.action_analysis = _.filter(data.data.action_analysis,  function(dat) { return dat.action_taken !== ComplaintStatusEnum.get_skip_label_index(); });
       });
 
       _.each($scope.complaint_view[0].data.action_analysis, function (value) {
-         $scope.patch_qsc_labels.push({action_name: Global.complaintAnalysisAction[value.action_taken][0], action_class: Global.complaintAnalysisActionClass[value.action_taken]});
+        $scope.patch_qsc_labels.push({action_name: Global.complaintAnalysisAction[value.action_taken][0], action_class: Global.complaintAnalysisActionClass[value.action_taken]});
       });
       $scope.patch_qsc_labels = _.sortBy($scope.patch_qsc_labels, function (value) { return Global.complaintAnalysisActionPriority[value.action_name];});
 
@@ -53,17 +52,17 @@
           _.each(value.data.action_analysis,function(dat){
             $scope.pakistan_analysis.push({ "category": Global.complaintAnalysisAction[dat.action_taken][0], "column-1": dat.count, "color": Global.complaintAnalysisAction[dat.action_taken][1] });
           });
-         }
-         else if(region_name === "South") {
+        }
+        else if(region_name === "South") {
           complaints = region_data(value.data.action_analysis);
           $scope.south_analysis.push({ "category": region_name.toUpperCase(), "column-1": complaints.Unprocessed, "column-2": complaints.Unrecoverable, "column-3": complaints.Recovered });
           $scope.north_south_percentage.push({ "category": region_name.toUpperCase(), "column-1": Math.round((value.data.feedback_count / pakistan_feedback_count) * 100), "color": "#ff0f00" });
-         }
-         else if(region_name === "North") {
+        }
+        else if(region_name === "North") {
           complaints = region_data(value.data.action_analysis);
           $scope.north_analysis.push({ "category": region_name.toUpperCase(), "column-1": complaints.Unprocessed, "column-2": complaints.Unrecoverable, "column-3": complaints.Recovered });
           $scope.north_south_percentage.push({ "category": region_name.toUpperCase(), "column-1": Math.round((value.data.feedback_count / pakistan_feedback_count) * 100), "color":"#ff6600" });
-         }
+        }
       });
       $scope.north_south_percentage = _.sortBy($scope.north_south_percentage, function (value) { return value.category; });
     }
