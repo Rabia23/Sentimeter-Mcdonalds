d0a1667e95999e3d2387e3ea9fbab42595e69960
abdullah <abdullah.khan.niazi@hotmail.com>
2016-03-31 19:05:21 +0500

    changed the urls

--- src/app/apiLinks.js
+++ src/app/apiLinks.js
@@ -5,18 +5,18 @@ angular.module('livefeed.api_links', [])
   return {
 
     link: {
-      api: "https://staginglivefeedapi.arbisoft.com/api/:endpoint/",
+      api: "https://stagingapimcdonalds.sentimeter.io/api/:endpoint/",
       analytics: function(){
         return $window.ga('create', 'UA-75485796-1', 'none');
       },
-      socket: "wss://staginglivefeed.arbisoft.com:5679/"
+      socket: "wss://stagingmcdonalds.sentimeter.io:5679/"
     }
     // link: {
-    //  api: "https://livefeed.arbisoft.com/api/:endpoint",
-    //  analytics: function(){
-    //    return $window.ga('create', 'UA-75485796-1', 'auto');
-    //  },
-    //  socket: "ws://livefeed.arbisoft.com:5679/"
+    //   api: "https://apimcdonalds.sentimeter.io/api/:endpoint",
+    //   analytics: function(){
+    //     return $window.ga('create', 'UA-75485796-1', 'auto');
+    //   },
+    //   socket: "wss://mcdonalds.sentimeter.io:5679/"
     // }
   };
 });
