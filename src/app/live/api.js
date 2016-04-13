582b6b507bde1abfa9099cf9f026befba78cb853
rabia23 <rabia.iftikhar@arbisoft.com>
2016-04-13 16:44:19 +0500

    add test for api-interceptor request header

--- src/app/live/api.js
+++ src/app/live/api.js
@@ -1,21 +1,23 @@
-angular.module( 'livefeed.live.api', [
-  'ngResource',
-  'livefeed.api_links'
-])
+(function() {
+  angular.module( 'livefeed.live.api', [
+    'ngResource',
+    'livefeed.api_links'
+  ])
 
-.factory('Api', ['$resource','apiLinks',  function($resource, apiLinks) {
+  .factory('Api', ['$resource','apiLinks',  function($resource, apiLinks) {
 
-  function Api() {
-    this.service = $resource(apiLinks.link.api, {},
-    {
-      live_dashboard: {method: "GET", isArray: false, params:{endpoint: "livedashboard"}}
+    function Api() {
+      this.service = $resource(apiLinks.link.api, {},
+      {
+        live_dashboard: {method: "GET", isArray: false, params:{endpoint: "livedashboard"}}
 
-    });
-  }
+      });
+    }
 
-  Api.prototype.live_dashboard = function(){
-    return this.service.live_dashboard();
-  };
+    Api.prototype.live_dashboard = function(){
+      return this.service.live_dashboard();
+    };
 
-  return new Api();
-}]);
+    return new Api();
+  }]);
+})();
