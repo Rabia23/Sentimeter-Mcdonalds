6e2952f8fa22b2dcd28ba678a5c8e9ec0d7f8105
rabia23 <rabia.iftikhar@arbisoft.com>
2016-04-25 15:25:44 +0500

    fixed margin issues in live dashboard and add count and calendar in opportunity analysis.

--- src/app/dashboard/opportunities/opportunities-controller.spec.js
+++ src/app/dashboard/opportunities/opportunities-controller.spec.js
@@ -14,48 +14,59 @@ describe('OpportunitiesCtrl', function(){
     window.ga = function(){};
   }));
 
-  it('success with data init $scope.opportunity_data', function(){
-    $httpBackend.whenGET(apilink)
-    .respond(
-      {
-        success: true,
-        response: {
-          "feedback_count": 46,
-          "feedbacks": [
-            {
-              "option__parent_id": null,
-              "option__color_code": "#4CCC72",
-              "option__score": 0,
-              "option__text": "Quality of Food",
-              "option_id": 37,
-              "count": 10
+  it('should define controller', function(){
+    expect(controller).toBeDefined();
+  });
+
+  it('init the datepicker options', function(){
+    expect($rootScope.datePickerOption).toBeDefined();
+  });
+
+  describe('resetDates method', function(){
+    it('reset dates', function(){
+      controller.resetDates();
+      expect($rootScope.date.startDate._d.getDate()).toBe(new Date().getUTCDate()-1);
+      expect($rootScope.date.endDate._d.getDate()).toBe(new Date().getUTCDate());
+    });
+
+  });
+
+  describe('drawOpportunityAnalysis method', function(){
+
+    it('success with data init $scope.opportunity_data', function(){
+      $httpBackend.whenGET(apilink)
+        .respond(
+          {
+            success: true,
+            response: {
+              "feedback_count": 46,
+              "feedbacks": [
+                {
+                  "option__parent_id": null,
+                  "option__color_code": "#4CCC72",
+                  "option__score": 0,
+                  "option__text": "Quality of Food",
+                  "option_id": 37,
+                  "count": 10
+                }
+              ]
             }
-          ]
-        }
-        
-      }
-    );
 
-    $httpBackend.flush();
-    expect($rootScope.opportunity_data[0].name).toEqual("Quality of Food");
+          });
+
+      $httpBackend.flush();
+      expect($rootScope.opportunity_data[0].name).toEqual("Quality of Food");
+    });
+
+    it('show flash when api request failure', function(){
+
+      spyOn(flashService, 'createFlash');
+
+      $httpBackend.whenGET(apilink)
+      .respond({ success: false });
+
+      $httpBackend.flush();
+      expect(flashService.createFlash).toHaveBeenCalled();
+    });
   });
-
-  it('show flash when api request failure', function(){
-    
-    spyOn(flashService, 'createFlash');
-    
-    $httpBackend.whenGET(apilink)
-    .respond(
-      {
-        success: false
-        
-      }
-    );
-
-    $httpBackend.flush();
-    expect(flashService.createFlash).toHaveBeenCalled();
-  });
-
-
-
 });
