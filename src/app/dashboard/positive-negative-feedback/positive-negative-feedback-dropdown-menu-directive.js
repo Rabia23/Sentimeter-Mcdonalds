d6a52684be08f5ad7bf685a10b43395126554ef5
rabia23 <rabia.iftikhar@arbisoft.com>
2016-05-04 12:50:21 +0500

    Merge branch 'frontend-develop' into feature/fixed-unit-test-issues

---
+++ src/app/dashboard/positive-negative-feedback/positive-negative-feedback-dropdown-menu-directive.js
@@ -0,0 +1,24 @@
+(function () {
+  angular.module('livefeed.dashboard.positive_negative_feedback')
+
+  .directive('dropMenu', function ($timeout) {
+    return {
+      restrict: 'A',
+      scope: {
+        branches: '='
+      },
+      link: function (scope, ele, attrs) {
+        scope.$watch('branches', function (branch) {
+          if (branch !== undefined) {
+            $('#single-button2').on('click', function () {
+              $timeout(function () {
+                window.initCustomForms();
+              }, 0.1);
+            });
+          }
+        });
+      }
+    };
+  });
+
+})();
